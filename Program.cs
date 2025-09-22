4c34658d8e6efb8827517dd6fb925e0eb7ccf8de
Florian Wiesauer <f.wiesauer@htlvb.at>
2025-09-22 08:13:36 +0200

    New feature #2

--- Program.cs
+++ Program.cs
@@ -4,3 +4,7 @@ Console.WriteLine("Hello World");
 
 Console.WriteLine("New amazing feature");
 if (1 + 1 == 2) { Console.WriteLine("Quick Math"); }
+
+Console.WriteLine("Pretty cool right?");
+
+Console.WriteLine("New amazing feature #2");
