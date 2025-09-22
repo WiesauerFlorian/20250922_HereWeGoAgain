7668fe27b24008b3c9fb5e9f1c2ca662567a5bfd
Florian Wiesauer <f.wiesauer@htlvb.at>
2025-09-22 08:08:13 +0200

    if-abfrage

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
-﻿Console.WriteLine("Hello World");
+﻿using System.Net.WebSockets;
+
+Console.WriteLine("Hello World");
 
 Console.WriteLine("New amazing feature");
+if (1 + 1 == 2) { Console.WriteLine("Quick Math"); }
